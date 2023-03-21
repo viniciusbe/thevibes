8b65e5b982afaff1162b08848e6294dfcbdf8513
Vinícius Bernardes <vinicius.bernardesds@gmail.com>
2023-03-21 19:29:43 -0300

    feat: create about section

---
+++ src/components/About.tsx
@@ -0,0 +1,25 @@
+import styles from '@/styles/About.module.css'
+
+export default function About() {
+  return (
+    <section className={styles.section} id="about">
+      <h1>About me</h1>
+      <p>
+        I&apos;m a web development located in Joinville-SC, south of Brazil. My
+        interest in programming started in 2016 when I had the first contact
+        with algorithm discipline on an Engineer course.
+      </p>
+      <p>
+        Since 2021, I&apos;ve worked for two local companies and now searching
+        for a remote job.
+      </p>
+      <p>
+        I believe that soft skills are as important as hard skills and
+        languages/stacks are just tools. Surfing, skating and hikinig are some
+        activies that I really enjoy doing
+      </p>
+
+      <p></p>
+    </section>
+  )
+}
