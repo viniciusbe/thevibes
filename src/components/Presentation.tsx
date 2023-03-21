fab57c200efbe817fe29c2838e0c349dd6d5c825
Vinícius Bernardes <vinicius.bernardesds@gmail.com>
2023-03-21 19:29:55 -0300

    feat: create presentation section

---
+++ src/components/Presentation.tsx
@@ -0,0 +1,11 @@
+import styles from '@/styles/Presentation.module.css'
+
+export default function Presentation() {
+  return (
+    <section className={styles.section}>
+      <h1>Vinícius Bernardes</h1>
+      <h2>Web developer</h2>
+      <p>Bringing technology, business and people together.</p>
+    </section>
+  )
+}
