9e0563128e9aca01acff817497484418ded9554b
Vinícius Bernardes <vinicius.bernardesds@gmail.com>
2023-03-21 19:39:22 -0300

    ref: about content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,9 +5,9 @@ export default function About() {
     <section className={styles.section} id="about">
       <h1>About me</h1>
       <p>
-        I&apos;m a web development located in Joinville-SC, south of Brazil. My
-        interest in programming started in 2016 when I had the first contact
-        with algorithm discipline on an Engineer course.
+        I&apos;m a web developer located in Joinville-SC, south of Brazil. My
+        interest in programming started in 2016 when I had my first contact with
+        the algorithm discipline on an Engineer course.
       </p>
       <p>
         Since 2021, I&apos;ve worked for two local companies and now searching
@@ -15,8 +15,8 @@ export default function About() {
       </p>
       <p>
         I believe that soft skills are as important as hard skills and
-        languages/stacks are just tools. Surfing, skating and hikinig are some
-        activies that I really enjoy doing
+        languages/stacks are just tools. Surfing, skating, and hiking are some
+        activities that I enjoy doing.
       </p>
 
       <p></p>
