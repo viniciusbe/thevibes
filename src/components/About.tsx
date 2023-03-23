afe6c90cef378148db49a4a6bf5ad97c353b3861
Vinícius Bernardes <vinicius.bernardesds@gmail.com>
2023-03-23 14:00:15 -0300

    feat: improve about and contact content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,21 +5,27 @@ export default function About() {
     <section className={styles.section} id="about">
       <h1>About me</h1>
       <p>
-        I&apos;m a frontend developer located in Joinville-SC, south of Brazil.
-        My interest in programming started in 2016 when I had my first contact
-        with the algorithm discipline on an Engineer course.
+        Hi, my name is Vinícius Bernardes and I am a frontend developer based in
+        Joinville-SC, located in the south of Brazil. My passion for programming
+        began in 2016 when I was first introduced to algorithmic principles
+        while pursuing an Engineering degree.
       </p>
       <p>
-        Since 2021, I&apos;ve worked for two local companies and now searching
-        for a remote job.
+        Over the past few years, I have gained valuable experience through my
+        work with two local companies. Now, I am eager to apply my skills and
+        knowledge to a remote position.
       </p>
       <p>
-        I believe that soft skills are as important as hard skills and
-        languages/stacks are just tools. Surfing, skating, and hiking are some
-        activities that I enjoy doing.
+        I believe that soft skills are just as important as hard skills, and
+        that programming languages and technology stacks are simply tools. In my
+        free time, I enjoy activities such as surfing, skating, and hiking,
+        which allow me to maintain a healthy work-life balance.
       </p>
-
-      <p></p>
+      <p>
+        I am excited about the prospect of collaborating with like-minded
+        individuals to create impactful projects and deliver innovative
+        solutions. Thank you for taking the time to learn a bit more about me.
+      </p>
     </section>
   )
 }
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -11,29 +11,41 @@ export default function Contact() {
     setIsCopied(true)
   }
 
+  const handleMouseOut = () => {
+    setShowTooltip(false)
+    setIsCopied(false)
+  }
+
   return (
     <section className={styles.section} id="contact">
       <h1>Get in touch</h1>
 
-      <p>Feel free to send an email:</p>
+      <p>
+        Interested in working with me or just want to chat about a project? Feel
+        free to get in touch via my email or my social media. I&apos;m looking
+        forward to hearing from you and seeing how I can help bring your vision
+        to life!
+      </p>
 
-      <div>
+      <div className={styles.email}>
         <a href="mailto:contact@viniciusbernardes.dev">
           contact@viniciusbernardes.dev
         </a>
-        <Image
-          onMouseOver={() => setShowTooltip(true)}
-          onMouseOut={() => setShowTooltip(false)}
-          src="/copyIcon.svg"
-          alt="Copy Icon"
-          width={25}
-          height={25}
-          priority
-          onClick={handleCopyEmail}
-        />
-        <span className={showTooltip ? styles.showTooltip : ''}>
-          {isCopied ? 'Copied to clipboard' : 'Copy to clipboard'}
-        </span>
+        <div>
+          <Image
+            onMouseOver={() => setShowTooltip(true)}
+            onMouseOut={handleMouseOut}
+            src="/copyIcon.svg"
+            alt="Copy Icon"
+            width={25}
+            height={25}
+            priority
+            onClick={handleCopyEmail}
+          />
+          <span className={showTooltip ? styles.showTooltip : ''}>
+            {isCopied ? 'Copied to clipboard' : 'Copy to clipboard'}
+          </span>
+        </div>
       </div>
     </section>
   )
